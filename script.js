ced15b5ccbbb1e9f6555f8e547122ab93298d043
olafvdg <olaf@brinkerink.nl>
2016-05-30 09:01:27 +0200

    First

---
+++ script.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+	var mydata = {
+			"name": "camera.startSession",
+			"parameters": {}
+		};
+		
+    $.ajax({
+		type: "POST",
+		url: 'http://192.168.1.1/osc/commands/execute',
+		headers: {
+			'Content-Type':'application/json;charset=utf-8',
+			'Accept': 'application/json',
+			'Content-Length': mydata.length
+		},
+		data: mydata,
+		success: function(data) {
+			console.log(data);
+		},
+		dataType: 'json'
+	});
+});
